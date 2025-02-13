1c31bb85ac45575473da96b367a85df9eb35ce0f
Ademar de Souza Lira Neto <ademarliraneto@gmail.com>
2025-02-12 21:50:37 -0300

    segundo commig

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$dbname = 'gestor-vendas';
+
+// Conectando ao banco de dados MySQL
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
