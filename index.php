882ac1f9ff721b99dad4bbff3a2d08c87e179543
Ademar de Souza Lira Neto <ademarliraneto@gmail.com>
2025-02-14 14:25:20 -0300

    subindo dashboard

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include_once('php/config.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/styleLogin.css">
+  <link rel="stylesheet" href="css/style.css">
   <title>Login</title>
 </head>
 
